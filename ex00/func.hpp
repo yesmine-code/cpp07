1c0c1fbebcfe6c2016b556db0b5bfd10b8f6de86
ybesbes <ybesbes@student.42.fr>
2022-01-28 13:04:40 +0100

    test main

--- ex00/func.hpp
+++ ex00/func.hpp
@@ -25,23 +25,5 @@ T	&max(T &a, T &b){
 	return b;
 }
 
-/*
-	class Awesome
-	{
-	public:
-	Awesome(void) : _n(0) {}
-	Awesome( int n ) : _n( n ) {}
-	Awesome & operator= (Awesome & a) { _n = a._n; return *this; }
-	bool operator==( Awesome const & rhs ) const { return (this->_n == rhs._n); }
-	bool operator!=( Awesome const & rhs ) const{ return (this->_n != rhs._n); }
-	bool operator>( Awesome const & rhs ) const { return (this->_n > rhs._n); }
-	bool operator<( Awesome const & rhs ) const { return (this->_n < rhs._n); }
-	bool operator>=( Awesome const & rhs ) const { return (this->_n >= rhs._n); }
-	bool operator<=( Awesome const & rhs ) const { return (this->_n <= rhs._n); }
-	int get_n() const { return _n; }
-	private:
-	int _n;
-	};
-	std::ostream & operator<<(std::ostream & o, const Awesome &a) { o << a.get_n(); return o; }*/
 
 #endif
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -7,19 +7,31 @@ int main(){
 	float f1 = 42.42f, f2 = 42.24f;
 	char  c1 = 'a', c2 = 'a';
 
-	swap(i1, i2);
+	::swap(i1, i2);
 	std::cout<< "i1 = "<< i1 <<"  i2 = "<<i2<<std::endl;
-	swap(f1, f2);
+	::swap(f1, f2);
 	std::cout<< "f1 = "<< f1 <<"  f2 = "<<f2<<std::endl;
 	std::cout<<"min f1 et f2 = (apres swap ;) = "<<min(f1, f2)<<std::endl;
 	std::cout<<"max i1 et i2 = (apres swap ;)= "<<max(i1, i2)<<std::endl;
 	std::cout<<"max s1 s2 = "<<max(s1, s2)<<std::endl;
 	std::cout<<"max c1 c2 = "<<max(c1, c2)<<std::endl;
-
-/*	Awesome a(2), b(4);
-	swap(a, b);
-	std::cout << a << " " << b << std::endl;
-	std::cout << max(a, b) << std::endl;
-	std::cout << min(a, b) << std::endl;*/
-
+	std::cout<<"subject main :" <<std::endl;
+	int a = 2;
+	int b = 3;
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+/*	a = 3, b = 2
+	min(a, b) = 2
+	max(a, b) = 3
+	c = chaine2, d = chaine1
+	min(c, d) = chaine1
+	max(c, d) = chaine2*/ //what should print
 }
